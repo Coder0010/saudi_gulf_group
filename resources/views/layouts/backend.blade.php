dae8fc28e4c1dabfd69df8de182895e8379b16f8
Mostafa Kamel Masoud <mostafakamel000@gmail.com>
2021-10-10 15:35:11 +0200

    ..

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @if (app()->isProduction())
-        <title>{{ config('app.name') }}</title>
+        <title>{{ config('app.name') }} | backend</title>
     @else
         <title>backend</title>
     @endif

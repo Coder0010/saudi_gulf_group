6115252b9a96100f8e1cabc54ab6664bda16357a
Mostafa Kamel Masoud <mostafakamel000@gmail.com>
2021-11-03 22:33:24 +0200

    ______

--- resources/views/frontend/services/show.blade.php
+++ resources/views/frontend/services/show.blade.php
@@ -56,7 +56,7 @@
                 </div>
             @endif
             @if (count($service->clients))
-                <div class="related-portfolio pt-0">
+                <div class="related-portfolio>
                     <div class="container">
                         <div class="row">
                             <div class="col-lg-12">

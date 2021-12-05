791c306823b3a89d1fdb3e72f44eb79c7a3bdc9f
Mostafa Kamel Masoud <mostafakamel000@gmail.com>
2021-12-05 22:54:54 +0200

    ______

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav id="nav">
     <ul class="main">
-        <li><a href="{{ route('frontend.index') }}">{{ __("translate.Home") }}</a></li>
-        <li><a href="{{ route('frontend.our-story') }}">{{ __("translate.Our-story") }}</a></li>
+        <li><a href="{{ route('frontend.index') }}">{{ __("translate.home") }}</a></li>
+        <li><a href="{{ route('frontend.our-story') }}">{{ __("translate.our-story") }}</a></li>
         @if ($services)
             <li>
-                <a data-toggle="collapse" href="#submenu1" role="button" aria-expanded="false" aria-controls="submenu1">{{ __("translate.Services") }}</a>
+                <a data-toggle="collapse" href="#submenu1" role="button" aria-expanded="false" aria-controls="submenu1">{{ __("translate.services") }}</a>
                 <ul class="collapse" id="submenu1">
                     @foreach ($services as $item)
                         <li><a href="{{ route('frontend.services.show', $item) }}">{{ $item->name }}</a></li>
@@ -13,8 +13,8 @@
             </li>
         @endif
         {{-- <li><a href="{{ route('frontend.portfolios.index') }}">Portfolios</a></li> --}}
-        <li><a href="{{ route('frontend.packages.index') }}">{{ __("translate.Packages") }}</a></li>
-        <li><a href="{{ route('frontend.contact-us') }}">{{ __("translate.Contact-us") }}</a></li>
+        <li><a href="{{ route('frontend.packages.index') }}">{{ __("translate.packages") }}</a></li>
+        <li><a href="{{ route('frontend.contact-us') }}">{{ __("translate.contact-us") }}</a></li>
         @if (App::isLocale('en'))
             <li><a href="{{ route('frontend.language', 'ar') }}" class="ar">عربي</a></li>
         @elseif (App::isLocale('ar'))
